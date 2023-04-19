aa8e9d7d169a8c3c32eec631ce46eb30c5e66b3b
iamSamuo <samuokim@gmail.com>
2023-04-19 12:39:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine" className="container">
+      <div id="display" className="display">
+        {props.sounds.map((key) => (
+          <Box text={key.key} key={key.key} audio={key.url} />
+        ))}
+      </div>
     </div>
   );
+};
+
+class Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.audio = React.createRef();
+    this.handlePlayAudio = this.handlePlayAudio.bind(this);
+  }
+
+  handlePlayAudio(e) {
+    this.audio.current.play();
+  }
+
+  render() {
+    const { text, audio } = this.props;
+
+    return (
+      <div className="box" onClick={this.handlePlayAudio}>
+        {text}
+        <audio ref={this.audio} className="clip" id={text} src={audio} />
+      </div>
+    );
+  }
 }
+
+document.addEventListener("keydown", (e) => {
+  const id = e.key.toUpperCase();
+  const audio = document.getElementById(id);
+
+  if (audio) {
+    const parent = audio.parentNode;
+    parent.classList.add("active");
+    audio.play();
+
+    audio.addEventListener("ended", () => {
+      parent.classList.remove("active");
+    });
+  }
+});
 
 export default App;
